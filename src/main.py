89518f95dc7d8cb436e1d03cda1733172d2cde91
uvsq22001322 <dsanghyeon@gmail.com>
2022-12-18 02:14:51 +0100

    Affichage adaptive Ok

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from reader import *
 import sys
 import os
 import time
+from tkinter import Tk, filedialog
 
 def un_pas(formel, mot):
     Tur = Machine(formel, mot)
@@ -33,21 +34,23 @@ def exec(formel, mot, affiche = True):
 
 
 def affichage(tur):
+    global TailleTerminal
     bandes = tur.get_bandes().get_list()
     for elem in bandes:
-        pointeur = ["---" if _ != 15 else " ^ " for _  in range(31)]
-        ruban = [" - " for _  in range(31)]
+        pointeur = ["---" if _ != (TailleTerminal // 5) // 2 else " ^ " for _  in range(TailleTerminal // 5)]
+        ruban = [" - " for _  in range(TailleTerminal // 5)]
         indice = 1
         for lettre in elem["mot"]:
-            ind = 15 - elem["pos"] + indice - 1
+            ind = (TailleTerminal // 10) - elem["pos"] + indice - 1
             #
             if ind < 0:
                 ind = 0
-            elif ind > 30:
-                ind = 30
+            elif ind > TailleTerminal // 5 - 1:
+                ind = TailleTerminal // 5 - 1
             #
             if lettre == "#" or lettre == "+":
                 lettre = "-"
+   
             ruban[ind] = " " + lettre + " "
             #print(ruban)
             indice += 1
@@ -56,31 +59,18 @@ def affichage(tur):
     print("\n")
 
 
-    """
-    for elem in bandes:
-        a = ["---" if _ != 15 else " ^ " for _  in range(31)]
-        b = [" - " for _  in range(31)]
-        i = 0
-        print(elem.get_mot())
-        for elem2 in "".join(map(str, elem.get_mot())).strip("+"):
-            
-            ind = 15 - elem.get_pos() + 1 + i
-            if ind < 0:
-                ind = 0
-            elif ind > 30:
-                ind = 30
-            if elem2 == "#":
-                elem2 = "-"
-            b[ind] = " " + elem2 + " "
-            i += 1
-
-        sys.stdout.write("/".join(map(str, b)) + "\n")
-        sys.stdout.write("-".join(map(str, a)) + "\n")
-    print("\n")
-    """
-
 
 if __name__ == "__main__":
-    formel = CodeTuring("turs\\TRI_BINAIRE.tur").get_auto()
+    #formel = CodeTuring("turs\\TRI_BINAIRE.tur").get_auto()
+    #exec(formel, "~10#11#11#00#01#11#00#11#11#00#01#11#00~")
+    size = os.get_terminal_size()
+    TailleTerminal = size.columns
+
+    root = Tk()
+    root.withdraw()
+
+    root.attributes('-topmost', True)
+    open_file = filedialog.askopenfilename(filetypes=[("Turing code", ".tur")])
+    formel = CodeTuring(open_file).get_auto()
     exec(formel, "~10#11#11#00#01#11#00#11#11#00#01#11#00~")
-    pass
+
