b87c85d86d30bbe233819dfa59bff614c2ad5fd9
uvsq22001322 <dsanghyeon@gmail.com>
2022-12-20 18:54:01 +0100

    linker Ok

--- src/linker.py
+++ src/linker.py
@@ -1,36 +1,26 @@
 from reader import *
-from turing import *
-from os import listdir
+from modifier import *
 
+def link(d, l):
+    # Pour tout les appelées
+    z = 0
+    for elem in l:
+        # Lire le code de l'appelée
+        readappel = reader("turs\\"+ elem["appel"] + ".tur")
+        new_dic = marqueur(readappel, str(z))
+        new_qi = new_dic["qi"]
+        new_qf = new_dic["qf"]
+        allchar = tuple(["%" for _ in range(d["nbr"])])
+        allmvt = tuple(["-" for _ in range(d["nbr"])])
+        # Transition de l'état ou est appelée
+        etatInit = elem["etat"]
+        d["dico"][etatInit][elem["lu"]] = {"dest": new_qi, "change": allchar, "mvt": elem["mvt"]}
+        # Transition du dernier état de l'appelée à "dest"
+        d["dico"][new_qf] = {}
+        d["dico"][new_qf][allchar] = {"dest": elem["dest"], "change": allchar, "mvt": allmvt}     
+        z += 1
+        # Ajout dans le dictionnaire principale
+        d["Qe"] = d["Qe"] + new_dic["Qe"]
+        d["dico"] = {**d["dico"], **new_dic["dico"]}
 
-# Choisie une machine qui appelle pour en créer une machine sans appel et créée un nouveau fichier .tur avec.
-def linker(auto):
-    listappel = []
-    for etat, sousdic in auto["dico"].items():
-        for lu, info in sousdic.items():
-            if type(info["change"]) != tuple:
-                listappel.append({"etat": etat, "lu": lu, "appelee": info["change"], "dest": info["dest"]})
-    
-    if not listappel:
-        print("La machine n'appelle aucune machine")
-        exit()
-    else:
-        a = listdir("turs")
-        possible = True
-        for elem in listappel:
-            if elem["appelee"]+".tur" not in a:
-                print("Il n'existe pas de fichier correspondant à la machine appelée: ",elem["appelee"]," dans le répertoire")
-                possible = False
-            
-        if possible:
-            link()
-        else:
-            print("Veuillez ajouter les fichiers *.tur nécessaire pour utiliser le linker.")
-    
-def link():
-    print("link")
-
-if __name__ == "__main__":
-    read = CodeTuring("turs\\TEST_LINKER.tur")
-    auto = read.get_auto()
-    linker(auto)
+    return d
